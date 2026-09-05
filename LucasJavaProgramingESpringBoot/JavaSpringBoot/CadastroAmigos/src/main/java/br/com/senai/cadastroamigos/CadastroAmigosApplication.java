189f36412657ca4fc2734c4b6027ee3689f93eb7
Lucas <lucaspietscher@gmail.com>
2026-09-05 16:48:16 -0300

    Adiconado Uma Arquitetura hexagonal no codigo e arrumando bug no codigo e adicionando documentação

---
+++ LucasJavaProgramingESpringBoot/JavaSpringBoot/CadastroAmigos/src/main/java/br/com/senai/cadastroamigos/CadastroAmigosApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.cadastroamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAmigosApplication.class, args);
+	}
+
+}
---
+++ LucasJavaProgramingESpringBoot/JavaSpringBoot/CadastroAmigos/src/test/java/br/com/senai/cadastroamigos/CadastroAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.cadastroamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
